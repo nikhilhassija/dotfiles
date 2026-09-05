87ca2e6c0a592b63f512c14e71b1732e4f905378
Nikhil <nikhil.hassija@gmail.com>
2026-09-05 19:42:12 +0530

    Update Neovim autoread configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,12 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+
+-- Enable autoread
+vim.o.autoread = true
+
+-- Trigger checktime to refresh buffers when Neovim gains focus or enters a buffer
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "silent! checktime",
+})
