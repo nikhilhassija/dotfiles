ec493f5b31f5ffdbd6399a832f625db220c03548
Nikhil <nikhil.hassija@gmail.com>
2026-09-20 20:09:59 +0530

    Set UTF-8 locale for tmux

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,3 +1,6 @@
 export EDITOR="nvim"
 export VISUAL="nvim"
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+export ENTE_CLI_SECRETS_PATH="$HOME/.config/ente/secrets.txt"
+export LANG=en_IN.utf8
+export LC_CTYPE=en_IN.utf8
